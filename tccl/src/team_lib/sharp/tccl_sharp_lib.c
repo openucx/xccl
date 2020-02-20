08d4b56fe879450c669e92448c0eff47fc27d6eb
Valentin Petrov <valentinp@mellanox.com>
2020-02-20 11:53:04 +0200

    tccl/sharp: refactor

--- tccl/src/team_lib/sharp/tccl_sharp_lib.c
+++ tccl/src/team_lib/sharp/tccl_sharp_lib.c
@@ -2,7 +2,8 @@
  * Copyright (C) Mellanox Technologies Ltd. 2020.  ALL RIGHTS RESERVED.
  * See file LICENSE for terms.
  */
-#include "tccl_team_lib_sharp.h"
+#include "tccl_sharp_lib.h"
+#include "tccl_sharp_collective.h"
 #include "tccl_sharp_map.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,11 +31,10 @@ static inline void tccl_sharp_global_rand_state_init()
     tccl_sharp_rand_state_init(&tccl_sharp_global_rand_state);
 }
 
-int tccl_team_sharp_oob_barrier(void *context)
+static int tccl_sharp_oob_barrier(void *context)
 {
     tccl_oob_collectives_t *oob = (tccl_oob_collectives_t*)context;
     int comm_size = oob->size;
-    int comm_rank = oob->rank;
     char *tmp = NULL, c = 'c';
     int rc;
 
@@ -44,8 +44,8 @@ int tccl_team_sharp_oob_barrier(void *context)
     return rc;
 }
 
-int tccl_team_sharp_oob_gather(void *context, int root, void *sbuf,
-                              void *rbuf, int size)
+static int tccl_sharp_oob_gather(void *context, int root, void *sbuf,
+                                 void *rbuf, int size)
 {
     tccl_oob_collectives_t *oob = (tccl_oob_collectives_t*)context;
     int comm_size = oob->size;
@@ -64,13 +64,13 @@ int tccl_team_sharp_oob_gather(void *context, int root, void *sbuf,
     return rc;
 }
 
-int tccl_team_sharp_oob_bcast(void *context, void *buf, int size, int root)
+static int tccl_sharp_oob_bcast(void *context, void *buf, int size, int root)
 {
     tccl_oob_collectives_t *oob = (tccl_oob_collectives_t*)context;
     int comm_size = oob->size;
     int comm_rank = oob->rank;
     void *tmp;
-    int rc; 
+    int rc;
 
     tmp = malloc(comm_size*size);
     rc = oob->allgather(buf, tmp, size, oob->coll_context);
@@ -82,53 +82,52 @@ int tccl_team_sharp_oob_bcast(void *context, void *buf, int size, int root)
 }
 
 static tccl_status_t
-tccl_team_sharp_create_context(tccl_team_lib_h team_lib,
-                              tccl_context_config_h config,
-                              tccl_context_h *team_context)
+tccl_sharp_create_context(tccl_team_lib_h lib, tccl_context_config_h config,
+                          tccl_context_h *context)
 {
-    tccl_team_sharp_context_t *team_sharp_ctx = malloc(sizeof(*team_sharp_ctx));
+    tccl_sharp_context_t *ctx = malloc(sizeof(*ctx));
     struct sharp_coll_init_spec init_spec = {0};
+    TCCL_CONTEXT_SUPER_INIT(ctx->super, lib, config);
 
     tccl_sharp_global_rand_state_init();
     map_tccl_to_sharp_dtype();
     map_tccl_to_sharp_reduce_op_type();
-    team_sharp_ctx->oob = config->oob;
 
     init_spec.progress_func                  = NULL;
-    init_spec.world_rank                     = team_sharp_ctx->oob.rank;
+    init_spec.world_rank                     = config->oob.rank;
     init_spec.world_local_rank               = 0;
-    init_spec.world_size                     = team_sharp_ctx->oob.size;
+    init_spec.world_size                     = config->oob.size;
     init_spec.enable_thread_support          = 1;
     init_spec.group_channel_idx              = 0;
-    init_spec.oob_colls.barrier              = tccl_team_sharp_oob_barrier;
-    init_spec.oob_colls.bcast                = tccl_team_sharp_oob_bcast;
-    init_spec.oob_colls.gather               = tccl_team_sharp_oob_gather;
-    init_spec.oob_ctx                        = &team_sharp_ctx->oob;
+    init_spec.oob_colls.barrier              = tccl_sharp_oob_barrier;
+    init_spec.oob_colls.bcast                = tccl_sharp_oob_bcast;
+    init_spec.oob_colls.gather               = tccl_sharp_oob_gather;
+    init_spec.oob_ctx                        = &ctx->super.cfg.oob;
     init_spec.config                         = sharp_coll_default_config;
     init_spec.config.user_progress_num_polls = 1000000;
     init_spec.config.ib_dev_list             = "mlx5_0:1";
     init_spec.job_id                         = tccl_sharp_rand();
-    tccl_team_sharp_oob_bcast((void*)&team_sharp_ctx->oob, &init_spec.job_id,
-                              sizeof(uint64_t), 0);
-    int ret = sharp_coll_init(&init_spec, &team_sharp_ctx->sharp_context);
+    tccl_sharp_oob_bcast((void*)&ctx->super.cfg.oob, &init_spec.job_id,
+                         sizeof(uint64_t), 0);
+    int ret = sharp_coll_init(&init_spec, &ctx->sharp_context);
     if (ret < 0 ) {
-        if (team_sharp_ctx->oob.rank == 0) {
-            fprintf(stderr, "Failed to initialize SHARP collectives:%s(%d)"  
+        if (config->oob.rank == 0) {
+            fprintf(stderr, "Failed to initialize SHARP collectives:%s(%d)"
                             "job ID:%" PRIu64"\n",
                             sharp_coll_strerror(ret), ret, init_spec.job_id);
         }
-        free(team_sharp_ctx);
+        free(ctx);
         return TCCL_ERR_NO_MESSAGE;
     }
-    *team_context = (tccl_context_h)team_sharp_ctx;
+    *context = &ctx->super;
     return TCCL_OK;
 }
 
 static tccl_status_t
-tccl_team_sharp_destroy_context(tccl_context_h team_context)
+tccl_sharp_destroy_context(tccl_context_h context)
 {
-    tccl_team_sharp_context_t *team_sharp_ctx =
-        (tccl_team_sharp_context_t *)team_context;
+    tccl_sharp_context_t *team_sharp_ctx =
+        tccl_derived_of(context, tccl_sharp_context_t);
     if (team_sharp_ctx->sharp_context) {
         sharp_coll_finalize(team_sharp_ctx->sharp_context);
     }
@@ -137,59 +136,55 @@ tccl_team_sharp_destroy_context(tccl_context_h team_context)
 }
 
 static tccl_status_t
-tccl_team_sharp_create_post(tccl_context_h team_context,
-                           tccl_team_config_h config,
-                           tccl_oob_collectives_t oob,
-                           tccl_team_h *team)
+tccl_sharp_team_create_post(tccl_context_h context,
+                            tccl_team_config_h config,
+                            tccl_oob_collectives_t oob,
+                            tccl_team_h *team)
 {
-    tccl_team_sharp_context_t *team_sharp_ctx =
-        (tccl_team_sharp_context_t *)team_context;
-    tccl_team_sharp_t *team_sharp             = malloc(sizeof(*team_sharp));
+    tccl_sharp_context_t *team_sharp_ctx =
+        tccl_derived_of(context, tccl_sharp_context_t);
+    tccl_sharp_team_t *team_sharp = malloc(sizeof(*team_sharp));
     struct sharp_coll_comm_init_spec comm_spec;
-    int i;
-    
-    team_sharp->oob = oob;
-    team_sharp->super.ctx = team_context;
+    int i, ret;
+    TCCL_TEAM_SUPER_INIT(team_sharp->super, context, config, oob);
 
     comm_spec.size              = oob.size;
     comm_spec.rank              = oob.rank;
     comm_spec.group_world_ranks = NULL;
-    comm_spec.oob_ctx           = &team_sharp->oob;
-    
-    int ret = sharp_coll_comm_init(team_sharp_ctx->sharp_context, &comm_spec,
-                                   &team_sharp->sharp_comm);
+    comm_spec.oob_ctx           = &team_sharp->super.oob;
+    ret = sharp_coll_comm_init(team_sharp_ctx->sharp_context, &comm_spec,
+                               &team_sharp->sharp_comm);
     if (ret<0) {
-        if (team_sharp->oob.rank == 0) {
+        if (oob.rank == 0) {
             fprintf(stderr, "SHARP group create failed:%s(%d)",
                     sharp_coll_strerror(ret), ret);
         }
         free(team_sharp);
         return TCCL_ERR_NO_MESSAGE;
     }
-    for(i = 0; i < TCCL_TEAM_SHARP_REG_BUF_NUM; i++) {
-        int rc;
-        team_sharp->bufs[i].buf = malloc(2*TCCL_TEAM_SHARP_REG_BUF_SIZE);
-        rc = sharp_coll_reg_mr(team_sharp_ctx->sharp_context,
+    for(i = 0; i < TCCL_SHARP_REG_BUF_NUM; i++) {
+        team_sharp->bufs[i].buf = malloc(2*TCCL_SHARP_REG_BUF_SIZE);
+        ret = sharp_coll_reg_mr(team_sharp_ctx->sharp_context,
                                team_sharp->bufs[i].buf,
-                               2*TCCL_TEAM_SHARP_REG_BUF_SIZE,
+                               2*TCCL_SHARP_REG_BUF_SIZE,
                                &team_sharp->bufs[i].mr);
-        if (rc != SHARP_COLL_SUCCESS) {
+        if (ret != SHARP_COLL_SUCCESS) {
             fprintf(stderr, "SHARP regmr failed\n");
         }
         team_sharp->bufs[i].used = 0;
     }
-    *team = (tccl_team_h)team_sharp;
+    *team = &team_sharp->super;
     return TCCL_OK;
 }
 
-static tccl_status_t tccl_team_sharp_destroy(tccl_team_h team)
+static tccl_status_t tccl_sharp_team_destroy(tccl_team_h team)
 {
-    tccl_team_sharp_t *team_sharp = (tccl_team_sharp_t*)team;
-    tccl_team_sharp_context_t *team_sharp_ctx =
-        (tccl_team_sharp_context_t *)team_sharp->super.ctx;
+    tccl_sharp_team_t *team_sharp = tccl_derived_of(team, tccl_sharp_team_t);
+    tccl_sharp_context_t *team_sharp_ctx =
+        tccl_derived_of(team->ctx, tccl_sharp_context_t);
 
     sharp_coll_comm_destroy(team_sharp->sharp_comm);
-    for(int i = 0; i < TCCL_TEAM_SHARP_REG_BUF_NUM; i++) {
+    for(int i = 0; i < TCCL_SHARP_REG_BUF_NUM; i++) {
         int rc;
         rc = sharp_coll_dereg_mr(team_sharp_ctx->sharp_context,
                                  team_sharp->bufs[i].mr);
@@ -210,14 +205,14 @@ tccl_team_lib_sharp_t tccl_team_lib_sharp = {
     .super.config.team_usage    = TCCL_USAGE_HW_COLLECTIVES,
     .super.config.coll_types    = TCCL_BARRIER | TCCL_ALLREDUCE,
     .super.ctx_create_mode      = TCCL_TEAM_LIB_CONTEXT_CREATE_MODE_GLOBAL,
-    .super.create_team_context  = tccl_team_sharp_create_context,
-    .super.destroy_team_context = tccl_team_sharp_destroy_context,
-    .super.team_create_post     = tccl_team_sharp_create_post,
-    .super.team_destroy         = tccl_team_sharp_destroy,
+    .super.create_team_context  = tccl_sharp_create_context,
+    .super.destroy_team_context = tccl_sharp_destroy_context,
+    .super.team_create_post     = tccl_sharp_team_create_post,
+    .super.team_destroy         = tccl_sharp_team_destroy,
     .super.progress             = NULL,
-    .super.collective_init      = tccl_team_sharp_collective_init,
-    .super.collective_post      = tccl_team_sharp_collective_post,
-    .super.collective_wait      = tccl_team_sharp_collective_wait,
-    .super.collective_test      = tccl_team_sharp_collective_test,
-    .super.collective_finalize  = tccl_team_sharp_collective_finalize
+    .super.collective_init      = tccl_sharp_collective_init,
+    .super.collective_post      = tccl_sharp_collective_post,
+    .super.collective_wait      = tccl_sharp_collective_wait,
+    .super.collective_test      = tccl_sharp_collective_test,
+    .super.collective_finalize  = tccl_sharp_collective_finalize
 };
---
+++ tccl/src/team_lib/sharp/tccl_sharp_lib.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) Mellanox Technologies Ltd. 2020.  ALL RIGHTS RESERVED.
+ * See file LICENSE for terms.
+ */
+#ifndef TCCL_TEAM_LIB_SHARP_H_
+#define TCCL_TEAM_LIB_SHARP_H_
+#include <sharp/api/version.h>
+#include <sharp/api/sharp_coll.h>
+#include "tccl_team_lib.h"
+
+#define TCCL_SHARP_REG_BUF_SIZE 1024
+#define TCCL_SHARP_REG_BUF_NUM  10
+
+typedef struct tccl_team_lib_sharp {
+    tccl_team_lib_t super;
+} tccl_team_lib_sharp_t;
+
+typedef struct tccl_sharp_context {
+    tccl_context_t             super;
+    struct sharp_coll_context *sharp_context;
+} tccl_sharp_context_t;
+
+typedef struct tccl_sharp_buf {
+    void *buf;
+    void *mr;
+    void *orig_src_buf;
+    void *orig_dst_buf;
+    int   used;
+} tccl_sharp_buf_t;
+
+typedef struct tccl_sharp_team {
+    tccl_team_t             super;
+    struct sharp_coll_comm *sharp_comm;
+    tccl_sharp_buf_t        bufs[TCCL_SHARP_REG_BUF_NUM];
+} tccl_sharp_team_t;
+#endif
