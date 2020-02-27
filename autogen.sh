c527b4ea246a7f38c521ecee7a4887dd39dd544c
Valentin Petrov <valentinp@mellanox.com>
2020-02-27 09:43:12 +0200

    remove mccl

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-echo "autogen: TCCL"
-cd tccl
-./autogen.sh
-cd ..
-echo "autogen: MCCL"
 rm -rf autom4te.cache
 mkdir -p config/m4 config/aux
-autoreconf -f --install || exit 1
+autoreconf -f  --install || exit 1
 rm -rf autom4te.cache
 exit 0
