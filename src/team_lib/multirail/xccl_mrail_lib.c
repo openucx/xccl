9d56f42ee1f0979355d22ec6f1f0cb943ddf87f9
Sergey Lebedev <sergey.a.lebedev@gmail.com>
2020-04-23 06:06:17 +0000

    TEAM/MRAIL: fix review comments

--- src/team_lib/multirail/xccl_mrail_lib.c
+++ src/team_lib/multirail/xccl_mrail_lib.c
@@ -80,7 +80,7 @@ static xccl_status_t xccl_mrail_init_tl(xccl_lib_h *lib_p)
         .team_usage = XCCL_LIB_PARAMS_TEAM_USAGE_SW_COLLECTIVES |
                       XCCL_LIB_PARAMS_TEAM_USAGE_HW_COLLECTIVES,
     };
-    
+
     status = xccl_lib_init(&params, NULL, lib_p);
 
     return status;
@@ -119,7 +119,7 @@ static xccl_status_t xccl_mrail_init_team(xccl_team_h *team, xccl_context_h ctx,
 {
     xccl_status_t status;
 
-    status = xccl_team_create_post(ctx, params, team); 
+    status = xccl_team_create_post(ctx, params, team);
     return status;
 }
 
@@ -134,7 +134,7 @@ xccl_mrail_context_create(xccl_team_lib_t *lib, xccl_context_params_t *params,
     xccl_status_t                  status;
     int                            i;
     int                            n_devs;
- 
+
     xccl_mrail_debug("Entering context create");
     cfg = ucs_derived_of(config, xccl_tl_mrail_context_config_t);
     n_devs = cfg->devices.count;
@@ -208,7 +208,6 @@ xccl_mrail_team_create_post(xccl_tl_context_t *context, xccl_team_params_t *para
     xccl_mrail_team_t    *team;
     xccl_status_t        status;
 
-    xccl_mrail_debug("Entering team create");
     team = (xccl_mrail_team_t*)calloc(1, sizeof(xccl_mrail_team_t));
     if (team == NULL){
         return XCCL_ERR_NO_MEMORY;
@@ -226,14 +225,16 @@ xccl_mrail_team_create_post(xccl_tl_context_t *context, xccl_team_params_t *para
 
     XCCL_TEAM_SUPER_INIT(team->super, context, params);
 
-    xccl_mrail_debug("Exiting team create");
     *tl_team = &team->super;
     return XCCL_OK;
 free_teams:
-    team->n_teams -= 1;
-    for(; team->n_teams >= 0; team->n_teams--) {
-        xccl_team_destroy(team->teams[team->n_teams]);
-    }
+/* TODO: destroy teams perhaps shouldn't be there because teams 
+ *       are not ready yet
+ *   team->n_teams -= 1;
+ *   for(; team->n_teams >= 0; team->n_teams--) {
+ *       xccl_team_destroy(team->teams[team->n_teams]);
+ *   }
+ */
     free(team);
     return status;
 }
@@ -263,21 +264,32 @@ free_teams:
         xccl_team_destroy(team->teams[team->n_teams]);
     }
     free(team);
+    return status;
 }
 
 xccl_status_t xccl_mrail_team_destroy(xccl_tl_team_t *team)
 {
-    xccl_mrail_team_t *tl = ucs_derived_of(team, xccl_mrail_team_t);
+    xccl_mrail_team_t *mrail_team = ucs_derived_of(team, xccl_mrail_team_t);
+    int               i;
 
-    xccl_mrail_debug("Entering team destroy");
-    free(tl);
-    xccl_mrail_debug("Exiting team destroy");
+    for(i = 0; i < mrail_team->n_teams; i++) {
+        xccl_team_destroy(mrail_team->teams[i]);
+    }
+
+    free(mrail_team);
 
     return XCCL_OK;
 }
 
 xccl_status_t xccl_mrail_context_progress(xccl_tl_context_t *team_context)
 {
+    int                  i;
+    xccl_mrail_context_t *mrail_ctx = ucs_derived_of(team_context,
+                                                     xccl_mrail_context_t);
+
+    for(i = 0; i < mrail_ctx->n_tls; i++) {
+        xccl_context_progress(mrail_ctx->tls[i]);
+    }
 
     return XCCL_OK;
 }
@@ -294,9 +306,10 @@ xccl_mrail_collective_init(xccl_coll_op_args_t *coll_args,
     int                   buf_len;
     int                   last_buf_len;
     int                   len;
-    xccl_mrail_debug("Entering collective init");
+    int                   offset;
+
     req = (xccl_mrail_coll_req_t*)malloc(sizeof(xccl_mrail_coll_req_t));
-    
+
     if (req == NULL) {
         return XCCL_ERR_NO_MEMORY;
     }
@@ -304,8 +317,7 @@ xccl_mrail_collective_init(xccl_coll_op_args_t *coll_args,
     if (coll_args->buffer_info.len < team->n_teams) {
         n_reqs  = 1;
         buf_len = 0;
-    }
-    else {
+    } else {
         n_reqs  = team->n_teams;
         buf_len = coll_args->buffer_info.len / n_reqs;
     }
@@ -315,17 +327,19 @@ xccl_mrail_collective_init(xccl_coll_op_args_t *coll_args,
     team_args = *coll_args;
 
     for(req->n_reqs = 0; req->n_reqs < n_reqs; req->n_reqs++) {
-        len = buf_len;
+        len    = buf_len;
+        offset = len*req->n_reqs;
         if (req->n_reqs == n_reqs - 1) {
             len = last_buf_len;
         }
-        xccl_mrail_debug("len %d \n", len);
-        team_args.buffer_info.dst_buffer = coll_args->buffer_info.dst_buffer +
-                                           len*req->n_reqs;
-        team_args.buffer_info.src_buffer = coll_args->buffer_info.src_buffer +
-                                           len*req->n_reqs;
+        team_args.buffer_info.dst_buffer = (void*)((ptrdiff_t)
+                                           coll_args->buffer_info.dst_buffer +
+                                           offset);
+        team_args.buffer_info.src_buffer = (void*)((ptrdiff_t)
+                                           coll_args->buffer_info.src_buffer +
+                                           offset);
         team_args.buffer_info.len        = len;
-        team_args.reduce_info.count      = len / xccl_dt_size(coll_args->reduce_info.dt);
+        team_args.reduce_info.count      = len/xccl_dt_size(coll_args->reduce_info.dt);
         status = xccl_collective_init(&team_args, &req->reqs[req->n_reqs],
                                       team->teams[req->n_reqs]);
         if (status != XCCL_OK) {
@@ -337,8 +351,6 @@ xccl_mrail_collective_init(xccl_coll_op_args_t *coll_args,
 
     req->super.lib = &xccl_team_lib_mrail.super;
     (*request) = &req->super;
-    xccl_mrail_debug("Exiting collective init");
-
     return XCCL_OK;
 free_req:
     free(req);
@@ -388,7 +400,6 @@ static xccl_status_t xccl_mrail_collective_wait(xccl_tl_coll_req_t *request)
 {
     xccl_status_t status;
 
-    xccl_mrail_debug("Entering collective wait");
     do {
         status = xccl_mrail_collective_test(request);
     } while (status == XCCL_INPROGRESS);
@@ -403,7 +414,6 @@ xccl_status_t xccl_mrail_collective_finalize(xccl_tl_coll_req_t *request)
     xccl_status_t         status;
     xccl_status_t         global_status;
 
-    xccl_mrail_debug("Entering collective finalize");
     global_status = XCCL_OK;
     for(i = 0; i < req->n_reqs; i++) {
         status = xccl_collective_finalize(req->reqs[i]);
@@ -413,7 +423,6 @@ xccl_status_t xccl_mrail_collective_finalize(xccl_tl_coll_req_t *request)
         }
     }
     free(request);
-    xccl_mrail_debug("Exiting collective finalize");
 
     return global_status;
 }
@@ -439,7 +448,7 @@ xccl_team_lib_mrail_t xccl_team_lib_mrail = {
                                    XCCL_THREAD_MODE_MULTIPLE,
     .super.params.team_usage     = XCCL_LIB_PARAMS_TEAM_USAGE_SW_COLLECTIVES,
     .super.params.coll_types     = XCCL_COLL_CAP_BARRIER   |
-                                   XCCL_COLL_CAP_BCAST     | 
+                                   XCCL_COLL_CAP_BCAST     |
                                    XCCL_COLL_CAP_ALLREDUCE,
     .super.ctx_create_mode       = XCCL_TEAM_LIB_CONTEXT_CREATE_MODE_LOCAL,
     .super.team_context_create   = xccl_mrail_context_create,
